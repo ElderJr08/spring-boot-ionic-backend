5d1a4bab10cd52a777867123235a6ea424b25577
Elder Junior <elderjunior08@gmail.com>
2018-07-25 21:41:09 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/com/elderjunior/cursomc/services/CategoriaService.java
+++ src/main/java/com/elderjunior/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.elderjunior.cursomc.domain.Categoria;
+import com.elderjunior.cursomc.dto.CategoriaDTO;
 import com.elderjunior.cursomc.repositories.CategoriaRepository;
 import com.elderjunior.cursomc.services.exceptions.DataIntegrityException;
 import com.elderjunior.cursomc.services.exceptions.ObjectNotFoundException;
@@ -58,6 +59,10 @@ public class CategoriaService {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy); 
 		return repo.findAll(pageRequest);
 		
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 
 }
